2f7e509133c36a0c1b957307a9da38e3bcd8d9fa
jananijayanthan1 <jananijayanthan1@gmail.com>
2025-07-10 18:29:48 +0100

    add spring project structure

---
+++ eaglebank-api/src/main/java/com/eaglebank/eaglebank_api/EaglebankApiApplication.java
@@ -0,0 +1,13 @@
+package com.eaglebank.eaglebank_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaglebankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaglebankApiApplication.class, args);
+	}
+
+}
